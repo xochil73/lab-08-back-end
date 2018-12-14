7c2d58027202f3a2cf4273b8550b958180c5abd0
Nikki1686 <nikki.l.carthen@gmail.com>
2018-12-13 21:28:55 -0800

    feat1 location now works

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
 //Application Dependencies
 const express = require('express');
 const cors = require('cors');
-const superAgent = require('superagent');
+const superagent = require('superagent');
 const pg = require('pg');
-// let lat;
-// let long;
+let lat;
+let long;
 
 //Load env vars;
 require('dotenv').config();
@@ -38,12 +38,12 @@ function getLocation (request, response) {
     })
   }
 }
-  lookupLocation(req.query.data, lookupHandler);
+  lookupLocation(request.query.data, lookupHandler);
 }
 
 function lookupLocation(query, handler){
-  const SQL = 'SELECT * FROM locations WHERE search_query=$1';
-  const values = [query];
+  const SQL = 'SELECT * FROM locations WHERE search_query=$1'
+  const values = [query]
   return client.query(SQL, values)
     .then(data => { // then if we have it, send it back;
       if (data.rowCount) {
@@ -51,6 +51,10 @@ function lookupLocation(query, handler){
       } else {
         handler.cacheMiss(query);
       }
+    })
+    .catch(err => {
+      console.error(err)
+    response.send(err)
     })
 }
 
@@ -74,34 +78,33 @@ function fetchLocation(query){
           .then(() => {
             return location;
           })
-       })
-
+      })
 }
 
 
 
 
 // Get weather data
-app.get('/weather', (request, response) => {
-  searchWeather(request.query.data /*|| 'Lynnwood, WA'*/)
-    .then(weatherData => {
-      response.send(weatherData);
-    });
-  // console.log(weatherGet);
-});
+// app.get('/weather', (request, response) => {
+//   searchWeather(request.query.data /*|| 'Lynnwood, WA'*/)
+//     .then(weatherData => {
+//       response.send(weatherData);
+//     });
+//   // console.log(weatherGet);
+// });
 //movies-----------------------------
-app.get('/movies', getMov);
-//mov func
-function getMov (request, response) {
-  return searchMovs(request.query.data)
-    .then(movData => {
-      response.send(movData);}
-    );
-}
+// app.get('/movies', getMov);
+// //mov func
+// function getMov (request, response) {
+//   return searchMovs(request.query.data)
+//     .then(movData => {
+//       response.send(movData);}
+//     );
+// }
 
 function searchMovs(query) {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`;
-  return superAgent.get(url)
+  return superagent.get(url)
     .then(moviesData => {
       // console.log(query);
       return moviesData.body.results.map(movie => new Movie(movie));
@@ -121,17 +124,17 @@ function Movie(movie) {
   this.released_on = movie.release_date;
 }
 //yelp-----------------------------------------------------------------------------------------------
-app.get('/yelp', getYelp);
+// app.get('/yelp', getYelp);
 
-function getYelp (request, response){
-  return searchYelps(request.query.data)
-    .then(yelpData => {
-      response.send(yelpData);}
-    );
-}
+// function getYelp (request, response){
+//   return searchYelps(request.query.data)
+//     .then(yelpData => {
+//       response.send(yelpData);}
+//     );
+// }
 function searchYelps(query) {
   const url = `https://api.yelp.com/v3/businesses/search?term=delis&latitude=${query.latitude}&longitude=${query.longitude}`;
-  return superAgent.get(url)
+  return superagent.get(url)
     .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
     .then(yelpData => {
       // console.log(yelpData.body.businesses);
@@ -150,7 +153,7 @@ function Bsns (bsns){
 // from class
 function searchToLatLong(query){
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-  return superAgent.get(url)
+  return superagent.get(url)
     .then(geoData => {
       const location = new Location(geoData.body.results[0]);
       // console.log(location);
@@ -167,7 +170,7 @@ function searchWeather(query){
   // body.results.geometry.location. lat || lng
   // console.log(url);
   // how to pull lat/long from google API, then format so we can input it into this URL
-  return superAgent.get(url)
+  return superagent.get(url)
     .then(weatherData => {
       let wArr = weatherData.body.daily.data.map(
         forecast => {
@@ -187,17 +190,17 @@ function Location(location, query){
   this.formatted_query = location.formatted_address;
   this.latitude = location.geometry.location.lat;
   this.longitude = location.geometry.location.lng;
-  // lat = location.geometry.location.lat;
-  // long = location.geometry.location.lng;
+  lat = location.geometry.location.lat;
+  long = location.geometry.location.lng;
 }
 
 // Error messages
-app.get('/*', function(req, res) {
-  res.status(404).send('halp, you are in the wrong place');
+app.get('/*', function(request, response) {
+  response.status(404).send('halp, you are in the wrong place');
 });
 
-function errorMessage(res){
-  res.status(500).send('something went wrong. plzfix.');
+function errorMessage(response){
+  response.status(500).send('something went wrong. plzfix.');
 } //created a function to handle the 500 errors but not sure what to do with it
 
 app.listen(PORT, () => {
